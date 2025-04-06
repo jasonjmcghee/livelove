f9126fe45325483178e7b3197c0c377a6fe79f26
Jason McGhee <mcghee.j@gmail.com>
2025-04-06 10:07:44 -0700

    Remove debug script

--- livelove.lua
+++ livelove.lua
@@ -590,16 +590,6 @@ livelove.last_working_draw = nil
 livelove.prev_draws = {}
 livelove.healing = false
 
-if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
-	local lldebugger = require "lldebugger"
-	lldebugger.start()
-	local run = love.run
-	function love.run(...)
-		local f = lldebugger.call(run, false, ...)
-		return function(...) return lldebugger.call(f, false, ...) end
-	end
-end
-
 function livelove.wrap_with_timeout(func, timeout)
     return function(...)
         local args = {...}
